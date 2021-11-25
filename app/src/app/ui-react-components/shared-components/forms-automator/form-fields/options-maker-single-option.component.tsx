ecb1f9af17b22ca2ea82137fb95f82edb2e6de9c
Don <4340360+ilDon@users.noreply.github.com>
2021-11-25 12:37:42 +0100

    Fixed new options initialized without options, and refatored

---
+++ app/src/app/ui-react-components/shared-components/forms-automator/form-fields/options-maker-single-option.component.tsx
@@ -0,0 +1,54 @@
+import { optionsBuilderForAdding, optionsBuilderForEditing } from 'app/data/form-models/options-builder.constant';
+import { OptionKeysModel } from 'app/data/model/form-model-commons';
+import { RESERVED_UDS_KEYS, Section } from 'app/data/model/project-info';
+import { AnitaStore } from 'app/libs/redux/reducers.const';
+import { DANGER_BTN_OUTLINE } from 'app/ui-react-components/shared-components/buttons/buttons-layout-tw-classes.const';
+import { FormAutomator } from 'app/ui-react-components/shared-components/forms-automator/form-automator.component';
+import { FormFieldsModel, ICommonFormEleProps } from 'app/ui-react-components/shared-components/forms-automator/form-fields/form-fields-model';
+import { useMemo } from 'react';
+import { useSelector } from 'react-redux';
+import ReactTooltip from 'react-tooltip';
+
+/**
+ * Checks if the OptionKeysModel was already in the section before we started editing.
+ * We can't rely on the general edit mode of the form because we might have new options in edit mode.
+ * @param section the section to which the current OptionKeysModel belongs
+ * @param indexFormElement the index of the form model at which the current OptionKeysModel is, as we want to check only on the same form model
+ * @param option the option to check
+ * @returns the form model to use. We want different form models for adding and editing to disable fields that should not be altered.
+ */
+function getCanEdit(section: Section, indexFormElement: number, value: string | number): boolean {
+  if (!section || !section.formModel[indexFormElement] || !section.formModel[indexFormElement]['options'] || !section.formModel[indexFormElement]['options'].length)
+    return true;
+
+  const exists = section.formModel[indexFormElement]['options'].some((opt: OptionKeysModel) => opt.value === value);
+  return !exists;
+}
+
+export const OptionsMakerSingleOption = (props: ICommonFormEleProps<FormFieldsModel<OptionKeysModel>>) => {
+  const { formEle, element, handleOptionsChange, handleClickDeleteOption, indexSection, indexFormElement, index, optionElement } = props;
+  const sections = useSelector((store: AnitaStore) => store.formProject.original[RESERVED_UDS_KEYS._sections]);
+  const formModelToUse: Array<FormFieldsModel<any>> = useMemo(() => {
+    const canEdit = getCanEdit(sections[indexSection], indexFormElement, optionElement.value);
+    return canEdit ? optionsBuilderForEditing : optionsBuilderForAdding;
+  }, [sections, indexSection, indexFormElement, optionElement.value]);
+
+  return (
+    <li>
+      <FormAutomator
+        formModel={formModelToUse}
+        element={optionElement}
+        handleChange={handleOptionsChange.bind(undefined, index, optionElement)} />
+      {(typeof handleClickDeleteOption === 'function' && element[formEle.fieldName].length > 2) && <div className="inline-block w-full lg:w-1/12 px-2 lg:pl-1 lg:pr-3 lg:align-bottom mb-8">
+        <button
+          onClick={handleClickDeleteOption.bind(undefined, index)}
+          data-tip data-for={`deleteOption-${index}`}
+          className={`w-full py-1 ${DANGER_BTN_OUTLINE}`}
+        ><i className="bi-trash hidden lg:block"></i><span className="lg:hidden">Delete option {index + 1}</span></button>
+        <ReactTooltip id={`deleteOption-${index}`} effect="solid">
+          <span>Delete option {index + 1}</span>
+        </ReactTooltip>
+      </div>}
+    </li>
+  )
+};
--- app/src/app/ui-react-components/shared-components/forms-automator/form-fields/options-maker.component.tsx
+++ app/src/app/ui-react-components/shared-components/forms-automator/form-fields/options-maker.component.tsx
@@ -1,57 +1,24 @@
-import { optionsBuilderForAdding, optionsBuilderForEditing } from 'app/data/form-models/options-builder.constant';
 import { OptionKeysModel } from 'app/data/model/form-model-commons';
-import { RESERVED_UDS_KEYS, Section } from 'app/data/model/project-info';
-import { AnitaStore } from 'app/libs/redux/reducers.const';
-import { DANGER_BTN_OUTLINE, SUCCESS_BTN_OUTLINE } from 'app/ui-react-components/shared-components/buttons/buttons-layout-tw-classes.const';
-import { FormAutomator } from 'app/ui-react-components/shared-components/forms-automator/form-automator.component';
+import { SUCCESS_BTN_OUTLINE } from 'app/ui-react-components/shared-components/buttons/buttons-layout-tw-classes.const';
 import { FormFieldsModel, ICommonFormEleProps } from 'app/ui-react-components/shared-components/forms-automator/form-fields/form-fields-model';
-import { useSelector } from 'react-redux';
-import ReactTooltip from 'react-tooltip';
+import { OptionsMakerSingleOption } from 'app/ui-react-components/shared-components/forms-automator/form-fields/options-maker-single-option.component';
 
-/**
- * Checks if the OptionKeysModel was already in the section before we started editing.
- * We can't rely on the general edit mode of the form because we might have new options in edit mode.
- * @param section the section to which the current OptionKeysModel belongs
- * @param indexFormElement the index of the form model at which the current OptionKeysModel is, as we want to check only on the same form model
- * @param option the option to check
- * @returns the form model to use. We want different form models for adding and editing to disable fields that should not be altered.
- */
-function getFormModelToUse(section: Section, indexFormElement: number, option: OptionKeysModel): Array<FormFieldsModel<OptionKeysModel>> {
-  if (!section || !section.formModel[indexFormElement] || !section.formModel[indexFormElement]['options'] || !section.formModel[indexFormElement]['options'].length)
-    return optionsBuilderForAdding;
+export const OptionsMaker = (props: ICommonFormEleProps<FormFieldsModel<OptionKeysModel>>) => {
 
-  const exists = section.formModel[indexFormElement]['options'].some((opt: OptionKeysModel) => opt.value === option.value);
-  return exists ? optionsBuilderForEditing : optionsBuilderForAdding;
-}
+  const { formEle, element, handleOptionsChange, handleClickAddOption } = props;
 
-export const OptionsMaker = ({ formEle, element, handleOptionsChange, handleClickAddOption, handleClickDeleteOption, indexSection, indexFormElement }: ICommonFormEleProps<FormFieldsModel<OptionKeysModel>>) => {
-
-  const sections = useSelector((store: AnitaStore) => store.formProject.original[RESERVED_UDS_KEYS._sections]);
-
-  if (!element[formEle.fieldName] || typeof handleOptionsChange !== 'function')
+  if (typeof handleOptionsChange !== 'function')
     return null;
+
+  if (!element[formEle.fieldName] || !Array.isArray(element[formEle.fieldName]) || !element[formEle.fieldName].length)
+    element[formEle.fieldName] = Array.from(Array(2), () => ({ label: '', value: '' }));
 
   return (
     <div className="mt-6 pl-10 py-4 bg-gray-50 rounded-lg">
       <p className="-ml-5 font-semibold mb-1">Choices</p>
       <ol className="list-decimal">
         {element[formEle.fieldName].map((optionElement: OptionKeysModel, index) =>
-          <li key={index}>
-            <FormAutomator
-              formModel={getFormModelToUse(sections[indexSection], indexFormElement, optionElement) as Array<FormFieldsModel<any>>}
-              element={optionElement}
-              handleChange={handleOptionsChange.bind(undefined, index, optionElement)} />
-            {(typeof handleClickDeleteOption === 'function' && element[formEle.fieldName].length > 1) && <div className="inline-block w-full lg:w-1/12 px-2 lg:pl-1 lg:pr-3 lg:align-bottom mb-8">
-              <button
-                onClick={handleClickDeleteOption.bind(undefined, index)}
-                data-tip data-for={`deleteOption-${index}`}
-                className={`w-full py-1 ${DANGER_BTN_OUTLINE}`}
-              ><i className="bi-trash hidden lg:block"></i><span className="lg:hidden">Delete option {index + 1}</span></button>
-              <ReactTooltip id={`deleteOption-${index}`} effect="solid">
-                <span>Delete option {index + 1}</span>
-              </ReactTooltip>
-            </div>}
-          </li>
+          <OptionsMakerSingleOption key={index} index={index} optionElement={optionElement} {...props} />
         )}
       </ol>
       {(typeof handleClickAddOption === 'function') &&
