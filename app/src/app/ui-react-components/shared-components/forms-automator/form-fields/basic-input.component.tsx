4ec8249bc7a40d07d8860b38c1cd944ec0c632a7
Don <4340360+ilDon@users.noreply.github.com>
2022-01-11 23:27:19 +0100

    improve: optimized with memo

--- app/src/app/ui-react-components/shared-components/forms-automator/form-fields/basic-input.component.tsx
+++ app/src/app/ui-react-components/shared-components/forms-automator/form-fields/basic-input.component.tsx
@@ -1,17 +1,22 @@
-import { AnitaStore } from 'app/libs/redux/reducers.const';
-import { REDUX_ACTIONS } from 'app/libs/redux/redux-actions.const';
-import { store } from 'app/libs/redux/state.store';
-import { ICommonFormEleProps } from 'app/ui-react-components/shared-components/forms-automator/form-automator.types';
-import { calcWidth } from 'app/ui-react-components/shared-components/forms-automator/form-builder/calc-width.function';
-import { FORM_ELEMENTS_CSS_CLASSES, FORM_ELEMENTS_CSS_CLASSES_ERR } from 'app/ui-react-components/shared-components/forms-automator/form-layout/fom-elements-css-classes.const';
-import { FormEleContainer } from 'app/ui-react-components/shared-components/forms-automator/form-layout/form-ele-container.component';
-import { FormElementLabel } from 'app/ui-react-components/shared-components/forms-automator/form-layout/form-element-label.component';
-import { ValidatorsContainer } from 'app/ui-react-components/shared-components/forms-automator/validators/validators-container.component';
-import uniqueId from 'lodash/uniqueId';
-import { useEffect, useRef, useState } from 'react';
-import { useSelector } from 'react-redux';
+import { AnitaStore } from 'app/libs/redux/reducers.const'
+import { REDUX_ACTIONS } from 'app/libs/redux/redux-actions.const'
+import { store } from 'app/libs/redux/state.store'
+import { ICommonFormEleProps } from 'app/ui-react-components/shared-components/forms-automator/form-automator.types'
+import { calcWidth } from 'app/ui-react-components/shared-components/forms-automator/form-builder/calc-width.function'
+import { FORM_ELEMENTS_CSS_CLASSES, FORM_ELEMENTS_CSS_CLASSES_ERR } from 'app/ui-react-components/shared-components/forms-automator/form-layout/fom-elements-css-classes.const'
+import { FormEleContainer } from 'app/ui-react-components/shared-components/forms-automator/form-layout/form-ele-container.component'
+import { FormElementLabel } from 'app/ui-react-components/shared-components/forms-automator/form-layout/form-element-label.component'
+import { ValidatorsContainer } from 'app/ui-react-components/shared-components/forms-automator/validators/validators-container.component'
+import uniqueId from 'lodash/uniqueId'
+import {
+  memo,
+  useEffect,
+  useRef,
+  useState
+  } from 'react'
+import { useSelector } from 'react-redux'
 
-export const BasicInput = ({ formEle, element, handleChange }: ICommonFormEleProps) => {
+export const BasicInput = memo(function BasicInput({ formEle, element, handleChange }: ICommonFormEleProps) {
 
   const [touched, setTouched] = useState(false);
   const { current: fieldId } = useRef(uniqueId(formEle.fieldName))
@@ -47,4 +52,6 @@ export const BasicInput = ({ formEle, element, handleChange }: ICommonFormElePro
       <ValidatorsContainer formEle={formEle} element={element} fieldId={fieldId} touched={touched} />
     </FormEleContainer>
   )
-};
+}, (prevProps, nextProps) => {
+  return prevProps.element[prevProps.formEle.fieldName] === nextProps.element[prevProps.formEle.fieldName];
+});
