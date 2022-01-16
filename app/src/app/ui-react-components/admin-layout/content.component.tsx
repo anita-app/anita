86bc85d465387f8fef304b40c393a8edc873d7e2
Don <4340360+ilDon@users.noreply.github.com>
2022-01-16 12:07:04 +0100

    improve: styled scrollbar

--- app/src/app/ui-react-components/admin-layout/content.component.tsx
+++ app/src/app/ui-react-components/admin-layout/content.component.tsx
@@ -1,7 +1,7 @@
-import { ReactNode } from 'react';
+import { ReactNode } from 'react'
 
 export const Content = (props: { children: ReactNode }) => (
-  <div className="flex-1 p-2 md:p-3 md:pt-5 lg:pt-5 lg:p-10 pt-5 overflow-auto">
+  <div className="flex-1 p-2 md:p-3 md:pt-5 lg:pt-5 lg:p-10 pt-5 overflow-auto main-scrollbar">
     {props.children}
   </div>
 );
