76e86e6f8c89f7cc6bffbb31cd0b81570c510f7d
Don <4340360+ilDon@users.noreply.github.com>
2022-08-27 14:13:03 +0200

    added comments

--- app/src/app/libs/project-helpers/project-handlers/project-exporter.ts
+++ app/src/app/libs/project-helpers/project-handlers/project-exporter.ts
@@ -12,6 +12,10 @@ export class ProjectExporter {
     private projectToExport: AnitaUniversalDataStorage
   ) {}
 
+  /**
+   * Ensures the SystemData is set in memory. If so, proceed with the logic.
+   * @remarks Static method to invoke this from the UI
+   */
   public static export(): void {
     const projectSystemData = store.getState().project
     if (projectSystemData) {
@@ -21,23 +25,35 @@ export class ProjectExporter {
     }
   }
 
+  /**
+   * Retrieves the data and saves it as a file on the device
+   */
   private async doExport(): Promise<void> {
     await this.getSectionsData()
     this.convertToJson()
     FsHelper.download(this.jsonData, `${this.projectToExport._settings[0].title}.json`, 'text/plain')
   }
 
+  /**
+   * Loop all sections
+   */
   private async getSectionsData(): Promise<void> {
     for (const section of this.projectToExport._sections) {
       await this.getSectionData(section.id)
     }
   }
 
+  /**
+   * Retrieve the data of each section and add it to the object to export
+   */
   private async getSectionData(sectionId: string): Promise<void> {
     const data = await dbInstances[this.projectToExport._settings[0].id].callSelector<SectionElement>(sectionId).multiple() || []
     this.projectToExport[sectionId] = data
   }
 
+  /**
+   * Data is exported in JSON
+   */
   private convertToJson(): void {
     this.jsonData = JSON.stringify(this.projectToExport, null, 2)
   }
