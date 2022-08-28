daf45a398d19be9c82664456c38a32939e3ba2b1
Don <4340360+ilDon@users.noreply.github.com>
2022-08-28 11:45:04 +0200

    refactor: moved function in component using it

--- app/src/app/ui-react-components/project/project-values-renderers/url.component.tsx
+++ app/src/app/ui-react-components/project/project-values-renderers/url.component.tsx
@@ -1,8 +1,8 @@
-import { removeProtocolFromUrl } from 'app/libs/tools/remove-protocol-from-url.function'
-
 interface IUrlProps {
   value: string
 }
+
+const removeProtocolFromUrl = (url: string) => url.replace(/^(?:https?:\/\/)?(?:www\.)?/i, '');
 
 export const Url: React.FC<IUrlProps> = ({ value }) => value
   ? <a href={value} target="_blank" rel="noreferrer" className="underline">{removeProtocolFromUrl(value)}</a>
