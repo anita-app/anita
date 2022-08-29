0d2dd217df3bdb48d69db2302adff41c7589b095
Don <4340360+ilDon@users.noreply.github.com>
2022-08-29 10:51:01 +0200

    4 columns

--- app/src/app/ui-react-components/project/project-grid/project-grid-list.tsx
+++ app/src/app/ui-react-components/project/project-grid/project-grid-list.tsx
@@ -13,7 +13,7 @@ export const ProjectGridList: React.FC<IProjectGridListProps> = (props) => {
   const titleKey = Manager.getCurrentProject().getSectionById(props.sectionId).getFirstUserDefinedField()?.fieldName
   const descriptionField = Manager.getCurrentProject().getSectionById(props.sectionId).getFirstFieldOfType([FORM_COMPONENTS_CODES.basicTextarea])
   return (
-    <div role="list" className="columns-1 gap-6 sm:columns-2 lg:columns-3">
+    <div role="list" className="columns-1 gap-6 sm:columns-2 lg:columns-3 xl:columns-4">
       {props.sectionData.map(element => (
         <ProjectGridListElement key={element.id} element={element} titleKey={titleKey} descriptionKey={descriptionField?.fieldName} />
       ))}
