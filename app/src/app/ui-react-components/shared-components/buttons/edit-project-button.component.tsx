7c619f92fc23226b6ae9a0011b4a4f431fa645e9
Don <4340360+ilDon@users.noreply.github.com>
2022-08-29 14:33:29 +0200

    added dropdown

--- app/src/app/ui-react-components/shared-components/buttons/edit-project-button.component.tsx
+++ app/src/app/ui-react-components/shared-components/buttons/edit-project-button.component.tsx
@@ -13,11 +13,14 @@ export const EditButton: React.FC<IEditButtonProps> = ({ project }) => (
   <Link
     to={urlParamFiller(ANITA_URLS.projectEdit, [{ name: URL_PARAMS.projectId, value: project.id }])}
     className="px-4 py-3 text-gray-800 mr-3 inline-flex items-center ml-auto leading-none text-sm bg-gray-100 hover:bg-gray-200 rounded"
-    data-tip={true} data-for="editProject"
+    data-tip={true}
+    data-for="editProject"
   >
     <i className="bi-pencil"></i> <span className="ml-2 hidden sm:inline-block">Edit Project</span>
-    <ReactTooltip id="editProject" effect="solid">
-      Edit project
-    </ReactTooltip>
+    <span className="sm:hidden">
+      <ReactTooltip id="editProject" effect="solid">
+        Edit project
+      </ReactTooltip>
+    </span>
   </Link>
 )
--- app/src/app/ui-react-components/shared-components/buttons/export-project-button.component.tsx
+++ app/src/app/ui-react-components/shared-components/buttons/export-project-button.component.tsx
@@ -1,8 +1,42 @@
 import { Manager } from 'app/libs/Manager/Manager.class'
 import ReactTooltip from 'react-tooltip'
 import React from 'react'
+import { Dropdown, IMenuItem } from 'app/ui-react-components/shared-components/common-ui-eles/dropdown.component'
+
+const exportMenuItems: Array<IMenuItem> = [
+  {
+    id: 'dataAndStructure',
+    label: 'Data and Structure',
+    handleClick: () => Manager.getCurrentProject().export()
+  },
+  {
+    id: 'dataOnly',
+    label: 'Data Only',
+    handleClick: () => Manager.getCurrentProject().export()
+  },
+  {
+    id: 'structureOnly',
+    label: 'Structure Only',
+    handleClick: () => Manager.getCurrentProject().export()
+  }
+]
 
 export const ExportButton: React.FC = () => (
+  <Dropdown
+    id="exportProject"
+    label="Export"
+    labelClassName="hidden md:inline-block"
+    tooltip="Export"
+    tooltipClassName='md:hidden'
+    chevronClassName="hidden md:inline-block"
+    icon="bi-arrow-bar-up"
+    iconClassName="md:hidden"
+    margin="mr-3"
+    menuItems={exportMenuItems}
+  />
+)
+
+export const ExportButtonO: React.FC = () => (
   <button
     className="px-4 py-3 text-gray-800 mr-3 inline-flex items-center ml-auto leading-none text-sm bg-gray-100 hover:bg-gray-200 rounded"
     data-tip={true} data-for="exportProject"
