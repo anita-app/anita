61ea893f6c84e3ed7ab3f8e09321c756be9c81d8
Don <4340360+ilDon@users.noreply.github.com>
2022-08-30 08:05:32 +0200

    show menu only if a project is loaded

--- app/src/app/Components/admin-layout/admin-layout.component.tsx
+++ app/src/app/Components/admin-layout/admin-layout.component.tsx
@@ -5,17 +5,24 @@ import { Content } from 'app/Components/admin-layout/content.component'
 import { Header } from 'app/Components/admin-layout/header.component'
 import { Sidebar } from 'app/Components/admin-layout/sidebar.component'
 import { HashRouter as Router } from 'react-router-dom'
+import { useSelector } from 'react-redux'
+import { AnitaStore } from 'app/libs/redux/reducers.const'
 
-export const AdminLayout: React.FC = () => (
+export const AdminLayout: React.FC = () => {
+  const project = useSelector((state: AnitaStore) => state.project)
+  return (
   <Router>
     <Header />
     <div className="relative admin-container flex">
-      <Sidebar>
-        <SidebarMenu />
-      </Sidebar>
+      {!!project && (
+        <Sidebar>
+          <SidebarMenu />
+        </Sidebar>
+      )}
       <Content>
         <AnitaRoutes />
       </Content>
     </div>
   </Router>
-)
+  )
+}
