f034fdc1302bfe74756ef4616da1490513a57b98
Don <4340360+ilDon@users.noreply.github.com>
2022-09-26 19:28:48 +0200

    aligned checkboxes

--- app/src/app/components/project/section/list/table/table-body-tr-td-without-link.component.tsx
+++ app/src/app/components/project/section/list/table/table-body-tr-td-without-link.component.tsx
@@ -6,7 +6,7 @@ interface IProjectSectionListTableTdWithoutLinkProps {
 
 export const ProjectSectionListTableBodyTrTdWithoutLink: React.FC<IProjectSectionListTableTdWithoutLinkProps> = (props) => (
   <td
-    className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6 md:pl-0"
+    className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6 md:pl-0 flex justify-center"
   >
     {props.children}
   </td>
--- app/src/app/components/project/section/list/table/table.component.tsx
+++ app/src/app/components/project/section/list/table/table.component.tsx
@@ -3,6 +3,7 @@ import { ISectionElement } from 'app/models/section-element/section-element.decl
 import { Section } from 'app/models/section/section.class'
 import { FormFieldsModel, TSupportedFormsTypes } from 'app/components/shared-components/forms-automator/form-automator.types'
 import { ProjectSectionListTableBodyTr } from 'app/components/project/section/list/table/table-body-tr.component'
+import { FORM_COMPONENTS_CODES } from 'app/components/shared-components/forms-automator/form-component-codes.enum'
 
 interface IProjectSectionListTableProps {
   section: Section
@@ -17,7 +18,8 @@ export const ProjectSectionListTable: React.FC<IProjectSectionListTableProps> = 
         {props.columns.map((col) => (
           <th
             key={`th-${col.fieldName}`}
-            className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6 md:pl-0"
+            // eslint-disable-next-line eqeqeq
+            className={`py-3.5 pl-4 pr-3 text-sm font-semibold text-gray-900 sm:pl-6 md:pl-0 ${col.componentCode != FORM_COMPONENTS_CODES.basicCheckbox ? 'text-left' : 'text-center'}`}
           >
             {col.label}
           </th>
