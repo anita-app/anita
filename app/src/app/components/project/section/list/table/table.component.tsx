9579b046e626a3c89ba0c14d483a00d13a696d79
Don <4340360+ilDon@users.noreply.github.com>
2022-09-27 14:16:26 +0200

    added sorting to table

--- app/src/app/components/project/section/list/table/table.component.tsx
+++ app/src/app/components/project/section/list/table/table.component.tsx
@@ -3,7 +3,7 @@ import { ISectionElement } from 'app/models/section-element/section-element.decl
 import { Section } from 'app/models/section/section.class'
 import { FormFieldsModel, TSupportedFormsTypes } from 'app/components/shared-components/forms-automator/form-automator.types'
 import { ProjectSectionListTableBodyTr } from 'app/components/project/section/list/table/table-body-tr.component'
-import { FORM_COMPONENTS_CODES } from 'app/components/shared-components/forms-automator/form-component-codes.enum'
+import { ProjectSectionListTableHeadTh } from 'app/components/project/section/list/table/table-head-th.component'
 
 interface IProjectSectionListTableProps {
   section: Section
@@ -16,13 +16,7 @@ export const ProjectSectionListTable: React.FC<IProjectSectionListTableProps> = 
     <thead>
       <tr>
         {props.columns.map((col) => (
-          <th
-            key={`th-${col.fieldName}`}
-            // eslint-disable-next-line eqeqeq
-            className={`py-3.5 pl-4 pr-3 text-sm font-semibold text-gray-900 sm:pl-6 md:pl-0 ${col.componentCode != FORM_COMPONENTS_CODES.basicCheckbox ? 'text-left' : 'text-center'}`}
-          >
-            {col.label}
-          </th>
+          <ProjectSectionListTableHeadTh key={`th-${col.fieldName}`} col={col} sectionId={props.section.id} />
         ))}
       </tr>
     </thead>
