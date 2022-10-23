704d26b0a43c4f8ad4e8cfc7e7c8d9fcdc8d9254
Don <4340360+ilDon@users.noreply.github.com>
2022-10-23 11:38:04 +0200

    improved navigation in file picker

--- app/src/app/components/admin-layout/header/cloud-sync-file-picker.tsx
+++ app/src/app/components/admin-layout/header/cloud-sync-file-picker.tsx
@@ -12,12 +12,16 @@ import React, { useEffect, useRef, useState } from 'react'
 const CloudSyncFilePicker: React.FC = () => {
   const [files, setFiles] = useState<Array<ISharedFileMeta> | null>(null)
   const [selected, setSelected] = useState<ISharedFileMeta | null>(null)
+  const [currentFolder, setCurrentFolder] = useState<ISharedFileMeta | null>(null)
+  const [isChangingFolder, setIsChangingFolder] = useState<boolean>(false)
+  const [direction, setDirection] = useState<'forward' | 'back'>('forward')
   const pathsHistoryRef = useRef<Array<string>>([])
   const { updateModal } = useModalContext()
   const getFiles = async () => {
     const path = pathsHistoryRef.current[pathsHistoryRef.current.length - 1]
     const files = await DropboxHelper.instance.getFileListForPath(path)
     setFiles(files)
+    setIsChangingFolder(false)
   }
 
   useEffect(() => {
@@ -25,37 +29,55 @@ const CloudSyncFilePicker: React.FC = () => {
   // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
 
-  const handleNavigateToFolder = async (path: string) => {
+  const handleNavigateToFolder = async (folder: ISharedFileMeta) => {
+    setDirection('forward')
+    setIsChangingFolder(true)
     setSelected(null)
-    pathsHistoryRef.current.push(path)
+    updateModal({ disableAction: true })
+    setCurrentFolder(folder)
+    pathsHistoryRef.current.push(folder.path!)
     await getFiles()
   }
 
   const handleSelectItem = async (file: ISharedFileMeta) => {
-    setSelected((currentValue) => currentValue?.id === file.id ? null : file)
-    updateModal({ disableAction: false })
+    setSelected((currentValue) => {
+      const newValue = currentValue?.id === file.id ? null : file
+      updateModal({ disableAction: !newValue })
+      return newValue
+    })
+  }
+
+  const handleSaveHere = (file: ISharedFileMeta | null) => {
+    console.log('handleSaveHere ~ file', file)
+    // todo
   }
 
   const goBack = async () => {
+    setDirection('back')
+    setIsChangingFolder(true)
     setSelected(null)
+    updateModal({ disableAction: true })
     pathsHistoryRef.current.pop()
     getFiles()
   }
 
   if (!files) {
-    return <div>Loading...</div>
+    return <div className="h-96">Loading...</div>
   }
 
   return (
     <>
       <FileExplorer
         files={files}
+        isChangingFolder={isChangingFolder}
+        direction={direction}
         selected={selected}
+        currentFolder={currentFolder}
         onSelecteItem={handleSelectItem}
         onNavigateToFolder={handleNavigateToFolder}
       />
-      {pathsHistoryRef.current.length > 0 && (
-        <div className="mt-1">
+      <div className="mt-1 h-8">
+        {pathsHistoryRef.current.length > 0 && (
           <Button
             id="file-picker-go-back"
             label="Back"
@@ -63,8 +85,16 @@ const CloudSyncFilePicker: React.FC = () => {
             iconLeft="chevronBack"
             onClick={goBack}
           />
-        </div>
-      )}
+        )}
+        {!pathsHistoryRef.current.length && (
+        <Button
+          id="save-here"
+          label="Save project in the root folder"
+          type={Type.transparent}
+          onClick={() => handleSaveHere(currentFolder)}
+        />
+        )}
+      </div>
       <Transition
         show={selected?.type === 'folder'}
         enter="ease-out duration-300"
@@ -76,8 +106,8 @@ const CloudSyncFilePicker: React.FC = () => {
       >
         <div className="pr-3 mt-4">
           <AlertWithDescription
-            title="Folder selected"
-            description={`The project file will be saved in the folder: ${selected?.name}`}
+            title={`Folder selected: ${selected?.name}`}
+            description="The project file will be saved in this folder as a new file."
             type={Type.success}
           />
         </div>
--- app/src/app/components/shared-components/file-explorer/file-explorer.tsx
+++ app/src/app/components/shared-components/file-explorer/file-explorer.tsx
@@ -1,13 +1,19 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { Transition } from '@headlessui/react'
+import { Button } from 'app/components/shared-components/common-ui-eles/button.component'
+import { Type } from 'app/components/shared-components/common-ui-eles/components.const'
 import { ISharedFileMeta } from 'app/libs/cloud-sync/cloud-sync.const'
 import { Icons } from 'app/libs/icons/icons.class'
 import React, { useEffect, useRef, useState } from 'react'
 
 interface IFileExplorerProps {
   files: Array<ISharedFileMeta>
+  isChangingFolder: boolean
+  direction: 'forward' | 'back'
   selected: ISharedFileMeta | null
+  currentFolder: ISharedFileMeta | null
   onSelecteItem: (file: ISharedFileMeta) => void
-  onNavigateToFolder: (path: string) => void
+  onNavigateToFolder: (folder: ISharedFileMeta) => void
 }
 
 const SUPPORTED_EXTENSIONS = ['json', 'db']
@@ -17,7 +23,6 @@ export const FileExplorer: React.FC<IFileExplorerProps> = (props) => {
   const [isScrollAtBottom, setIsScrollAtBottom] = useState(false)
 
   useEffect(() => {
-    // scroll containerRef.current to top
     if (containerRef.current) {
       containerRef.current.scrollTop = 0
     }
@@ -40,48 +45,68 @@ export const FileExplorer: React.FC<IFileExplorerProps> = (props) => {
     })
   }, [])
 
+  const enterFrom = props.direction === 'back' ? '-translate-x-full' : 'translate-x-full'
+  const leaveTo = props.direction === 'back' ? 'translate-x-full' : '-translate-x-full'
+  const navigableFiles = props.files?.filter((file) => file.type === 'folder' || SUPPORTED_EXTENSIONS.includes(file.name.split('.').pop()?.toLowerCase() || ''))
   return (
     <div className="relative">
-      <div ref={containerRef} className="bg-white max-h-96 overflow-y-auto pr-2">
+      <div ref={containerRef} className="bg-white h-96 overflow-y-auto overflow-x-hidden pr-2">
         <Transition
-          show={!!props.files.length}
-          enter="ease-out duration-300"
-          enterFrom="opacity-0"
-          enterTo="opacity-100"
-          leave="ease-in duration-200"
-          leaveFrom="opacity-100"
-          leaveTo="opacity-0"
+          show={!props.isChangingFolder}
+          enter="h-full ease-out duration-300"
+          enterFrom={enterFrom}
+          enterTo="translate-x-0"
+          entered="h-full"
+          leave="h-full ease-in duration-200"
+          leaveFrom="translate-x-0"
+          leaveTo={leaveTo}
         >
-          <ul role="list" className="divide-y divide-gray-200 border rounded-md">
-            {props.files
-              .filter((file) => file.type === 'folder' || SUPPORTED_EXTENSIONS.includes(file.name.split('.').pop()?.toLowerCase() || ''))
-              .map((file) => (
-                <li key={file.id}>
-                  <button className="w-full leading-none">
-                    <div className="flex items-center">
-                      <div
-                        className={`py-4 pl-4 min-w-0 flex-1 sm:flex sm:items-center sm:justify-between ${props.selected?.id === file.id ? 'bg-blue-50' : 'hover:bg-gray-50'}`}
-                        onClick={() => props.onSelecteItem(file)}
-                      >
-                        <div className="truncate">
-                          <div className="flex text-sm">
-                            <i className="mr-3">{Icons.render(file.type === 'file' ? 'documentOutline' : 'folderOutline')}</i><p className="truncate font-medium text-gray-800">{file.name}</p>
+          {!!navigableFiles.length && (
+            <ul role="list" className="divide-y divide-gray-200 border rounded-md">
+              {navigableFiles
+                .map((file) => (
+                  <li key={file.id}>
+                    <button className="w-full leading-none">
+                      <div className="flex items-center">
+                        <div
+                          className={`py-4 pl-4 min-w-0 flex-1 sm:flex sm:items-center sm:justify-between ${props.selected?.id === file.id ? 'bg-blue-50' : 'hover:bg-gray-50'}`}
+                          onClick={() => props.onSelecteItem(file)}
+                        >
+                          <div className="truncate">
+                            <div className="flex text-sm">
+                              <i className="mr-3">{Icons.render(file.type === 'file' ? 'documentOutline' : 'folderOutline')}</i><p className="truncate font-medium text-gray-800">{file.name}</p>
+                            </div>
                           </div>
                         </div>
+                        {file.type === 'folder' && (
+                        <div className="py-4 pr-4 flex-shrink-0 border-l pl-4 hover:bg-gray-100 leading-none h-[52px]" onClick={() => props.onNavigateToFolder(file)}>
+                          <i className="h-5 w-5 text-gray-400" aria-hidden="true">{Icons.render('chevronForward')}</i>
+                        </div>
+                        )}
                       </div>
-                      {file.type === 'folder' && (
-                      <div className="py-4 pr-4 flex-shrink-0 border-l pl-4 hover:bg-gray-100 leading-none h-[52px]" onClick={() => props.onNavigateToFolder(file.path!)}>
-                        <i className="h-5 w-5 text-gray-400" aria-hidden="true">{Icons.render('chevronForward')}</i>
-                      </div>
-                      )}
-                    </div>
-                  </button>
-                </li>
-              ))}
-          </ul>
+                    </button>
+                  </li>
+                ))}
+            </ul>
+          )}
+          {!navigableFiles.length && props.currentFolder && (
+            <div className="flex items-center justify-center h-full">
+              <div className="text-gray-500 text-center">
+                <i className="h-10 w-10" aria-hidden="true">{Icons.render('folderOutline')}</i>
+                <p className="mt-2 text-sm">No files or folders found</p>
+                <Button
+                  id="save-project-here"
+                  label="Save project here"
+                  onClick={() => props.onSelecteItem(props.currentFolder!)}
+                  marginClassName="mt-4"
+                  type={Type.primary}
+                />
+              </div>
+            </div>
+          )}
         </Transition>
       </div>
-      {!isScrollAtBottom && !!props.files?.length && (
+      {!isScrollAtBottom && !!navigableFiles?.length && (
         <div className="bottom-0 h-10 pointer-events-none absolute inset-x-0 z-10 bg-gradient-to-b from-transparent to-white"></div>
       )}
     </div>
