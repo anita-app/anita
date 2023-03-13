20471b10c605ddd6df2d4aac6efbc50ba0fc754c
Don <4340360+ilDon@users.noreply.github.com>
2023-03-13 19:12:27 +0100

    auto scroll

--- app/src/app/components/admin-layout/assistant/assistant-toolbar-responses-list.tsx
+++ app/src/app/components/admin-layout/assistant/assistant-toolbar-responses-list.tsx
@@ -7,11 +7,22 @@ interface IAssistantToolbarResponsesListProps {
 }
 
 export const AssistantToolbarResponsesList: React.FC<IAssistantToolbarResponsesListProps> = (props) => {
+  const scrollableRef = React.useRef<HTMLDivElement>(null)
+
+  React.useEffect(() => {
+    if (scrollableRef.current) {
+      scrollableRef.current.scrollTo({
+        top: scrollableRef.current.scrollHeight,
+        behavior: 'smooth'
+      })
+    }
+  }, [props.responses])
+
   if (!props.responses?.length) {
     return null
   }
   return (
-    <div className="flex flex-col max-h-full p-4 pt-0 overflow-auto">
+    <div ref={scrollableRef} className="flex flex-col max-h-full p-4 pt-0 overflow-auto">
       <ul>
         {props.queries.map((query, index) => (
           <AssistantToolbarResponsesListItem key={index} response={props.responses[index]} query={query} />
