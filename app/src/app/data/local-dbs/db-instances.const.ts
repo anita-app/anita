c6cc645770a0e142efd5093564de1f18146bbf7f
Don <4340360+ilDon@users.noreply.github.com>
2026-01-09 13:46:35 +0100

    chore: removed deta

--- app/src/app/data/local-dbs/db-instances.const.ts
+++ app/src/app/data/local-dbs/db-instances.const.ts
@@ -1,12 +1,11 @@
 import { TAnitaUniversalDataStorage } from 'app/models/project/project.declarations'
 import { DbConnectorInstance } from 'app/libs/db-connector/models/executers'
-import Base from 'deta/dist/types/base'
 import Dexie from 'dexie'
 import { Database } from 'sql.js'
 
 interface DbInstances {
   system: DbConnectorInstance<Dexie>
-  [projectIdentifier: string]: DbConnectorInstance<Dexie | Base | TAnitaUniversalDataStorage | Database>
+  [projectIdentifier: string]: DbConnectorInstance<Dexie | TAnitaUniversalDataStorage | Database>
 }
 
 export const dbInstances: DbInstances = {
