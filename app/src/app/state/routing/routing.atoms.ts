2e675258dadd933f5f2690093cf3717d37b2500f
Don <4340360+ilDon@users.noreply.github.com>
2026-01-09 20:59:58 +0100

    feat: atom family for param

--- app/src/app/state/routing/routing.atoms.ts
+++ app/src/app/state/routing/routing.atoms.ts
@@ -1,7 +1,9 @@
-import { IRouteParams, TAnitaRoute } from 'app/libs/routing/anita-routes.constant'
-import { atom, PrimitiveAtom } from 'jotai'
+import { IRouteParams, TAnitaRoute, URL_PARAMS } from 'app/libs/routing/anita-routes.constant'
+import { Atom, atom, PrimitiveAtom } from 'jotai'
+import { AtomFamily, atomFamily } from 'jotai-family'
 
 export class RoutingAtoms {
   public static route: PrimitiveAtom<TAnitaRoute | null> = atom(null) as PrimitiveAtom<TAnitaRoute | null>
   public static params: PrimitiveAtom<IRouteParams | null> = atom(null) as PrimitiveAtom<IRouteParams | null>
+  public static param: AtomFamily<URL_PARAMS, Atom<string | undefined>> = atomFamily((param: URL_PARAMS) => atom(get => get(this.params)?.[param]))
 }
