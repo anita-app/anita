940d62c603007bafa5439bc6563d4cf34cfb3d46
Don <4340360+ilDon@users.noreply.github.com>
2026-01-09 14:06:45 +0100

    refactor: react 19

--- app/config-overrides.js
+++ app/config-overrides.js
@@ -2,6 +2,21 @@ const NodePolyfillPlugin = require('node-polyfill-webpack-plugin')
 const ModuleScopePlugin = require('react-dev-utils/ModuleScopePlugin')
 
 module.exports = function override (config) {
+  const rulesWithOneOf = config.module.rules.find(rule => Array.isArray(rule.oneOf))
+  const babelLoader = rulesWithOneOf?.oneOf?.find(
+    rule => rule.loader && rule.loader.includes('babel-loader') && rule.options
+  )
+  if (babelLoader) {
+    const compilerPlugin = require.resolve('babel-plugin-react-compiler')
+    const plugins = babelLoader.options.plugins || []
+    const hasCompilerPlugin = plugins.some(
+      plugin => plugin === compilerPlugin || (Array.isArray(plugin) && plugin[0] === compilerPlugin)
+    )
+    if (!hasCompilerPlugin) {
+      babelLoader.options.plugins = [...plugins, compilerPlugin]
+    }
+  }
+
   config.plugins.push(
     new NodePolyfillPlugin()
   )
